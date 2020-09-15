f6071a80c48c8cd1a04dc2620d78439ad21996b4
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-15 05:20:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  person = {
+  {
     name: "Jon Snow",
     occupation: "King of The North"
   }
@@ -13,7 +13,7 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  person = {
+  {
     name: "Grace Hopper"
   }
 end
