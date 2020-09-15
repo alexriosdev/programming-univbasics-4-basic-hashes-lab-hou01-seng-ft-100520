437b8cccb2839435135092e36ffd8f6af1f6ab82
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-15 05:18:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,14 +5,21 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  human = { A
+  person = {
+    name: "Jon Snow",
+    occupation: "King of The North"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  person = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id: #{number}
 end
